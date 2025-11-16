8801e06f6c2b5ab8cd0882540f96f01b45207648
yosuke.kohno <yosuke.kohno@ankerinnovation.com>
2025-11-17 00:57:26 +0900

    fix: add _document.tsx and layout container to _app.tsx

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,5 +2,11 @@ import type { AppProps } from 'next/app';
 import '../app.unused/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="max-w-7xl mx-auto p-4 sm:p-6 lg:p-8">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
